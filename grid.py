d188c6fc996fcceff41389600a6a8a91e6c7445d
XavierProspero <xavier@spatialinc.com>
2020-12-24 13:32:01 -0800

    parse gride from a file

--- grid.py
+++ grid.py
@@ -1,58 +1,58 @@
 """
-The grid is made of cells.
-Some cells are starts of words.
+    The grid in which the crossword puzzle resides.
+    Assumes grid is N x N.
 
-All indexing is row column.
+    A well formatted grid file looks like.
 
-Use Public functions to manipulate cell.
+    3
+    w w w
+    w b w
+    w w w
 """
-from dataclasses import dataclass
+from cell import Cell, StartCell
+from utils import print_info, print_debug
 
-@dataclass
-class Step:
-    x: int
-    y: int
+class Grid:
 
-class Cell:
-
-    def __init___(self, is_black, pos):
-        # Local variables
-        self.is_start = False
-        self.letter = None
-        self.is_black = is_black
-        self.pos = pos
+    def __init__(self, file):
+        # Create empty cells
+        self.__init_grid_from_file(file)
 
 # Public
-    def GetIsBlack(self):
-        return self.is_black
 
-    def GetLetter(self, letter):
-        return self.letter
+# Private
+    def __init_grid_from_file(self, file):
+        BLACK = "b"
+        WHITE = "w"
 
-    def SetLetter(self, letter):
-        self.letter = letter
+        print_info("reading file named: {}".format(file))
+
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            print_debug(lines)
+            N = int(lines[0])
+
+            if N is not (len(lines) - 1):
+                print_info("init_grid_from_file: received malformed crossword file.")
+            else:
+                self.grid = [[[] for _ in range(N)] for _ in range(N)]
+                self.N = N
+
+                for line_idx, line in enumerate(lines[1:]):
+
+                    characters = line.split(" ")
+
+                    if len(characters) is not N:
+                        print_info("init_grid_from_file: received malformed crossword file.")
+                    else:
+                        for c_idx, c in enumerate(characters):
+                            is_black = (c is BLACK)
+                            pos = (line_idx, c_idx)
+                            self.grid[line_idx][c_idx] = Cell(is_black, pos)
+
+        print_info("succesfully read file: {}".format(file))
+        print_info("puzzle: {}".format(self.grid))
 
 
-class StartCell(Cell):
-
-    def __init__(self, cell):
-
-        #  These are Steps
-        self.next_letterX = None
-        self.next_letterY = None
-
-        self.word = None
-
-# Public
-    def GetNumWords(self):
-        retval = 0
-
-        if self.next_letterX:
-            retval += 1
-        if self.next_letterY:
-            retval += 1
-
-        return retval
-
-    def GetWord(self):
-        return word
+# Test
+Grid("test.puzzle")
